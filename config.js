a5b252a8310734b107370255e22009759f48e7c8
sohanlakra1980-sys <sohanlakra1980@gmail.com>
2026-08-28 15:20:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Replace this with your deployed Apps Script Web App URL ending in /exec
+window.BTS_CONFIG = {
+  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbxK5G-1BXsw3vGRKtVxnXLwRIToK0aVKOK6f9zjjUIT8xojUGjuyYXMVrz0-HDtQyE/exec'
+};
---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='bsnl-bts-v5-shell-1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin && e.request.method==='GET'){
+    e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return r}).catch(()=>caches.match('./index.html'))));
+  }
+});
